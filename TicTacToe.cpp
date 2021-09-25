c3659f52c7817875195751cab2bd8178a575c560
patCU <70227836+patCU@users.noreply.github.com>
2021-09-24 20:24:15 -0600

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,21 +15,7 @@ int** PlaceMarker(int** a, int r, int c, int val){
     a[r][c] = val;
     return a;
 }
-int* userChoice(char player){
-    int rowInput;
-    cout << "Player: " << player << " please select row: ";
-    cin >> rowInput;
-    
-    int colInput;
-    cout << "please select col: ";
-    cin >> colInput;
-    cout << " " << endl;
-    
-    int* temp = new int[2];
-    temp[0] = rowInput;
-    temp[1] = colInput;
-    return temp;
-}
+
 
 void displayBoard(int** a){
     for(int i=0;i<3;i++){
